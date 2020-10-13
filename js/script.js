7f78f9b5782ba4d9d11938f148dcb0e2816ff90b
Max Voitsekhovsky <voitsekhovskymax@gmail.com>
2020-10-13 15:17:59 +0300

    Store Page, new assets

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	console.log("ready!");
-	
+
 	// City Selector
 	var state_menu = false;
 	$(".city-drop-main").click(function () {
@@ -11,14 +11,14 @@ $(document).ready(function () {
 		else
 			list.removeClass("w--open");
 	});
-	
-	
+
+
 	// Menu
-	
+
 	$(".hamburger-menu, .menu-navbar .close").click(function () {
 		$(".menu-navbar").toggleClass("open");
 	});
-	
+
 	// Schedule Carousel
 	let stagePadding = 30;
 	let center = true;
@@ -26,13 +26,13 @@ $(document).ready(function () {
 		center = false;
 		stagePadding = 0;
 	}
-	
+
 	var owl = $(".owl-carousel.schedule-carousel").owlCarousel({
 		loop: false,
 		margin: 10,
 		center: center,
 		nav: true,
-		
+
 		stagePadding: stagePadding,
 		responsive: {
 			0: {
@@ -49,24 +49,24 @@ $(document).ready(function () {
 			}
 		}
 	});
-	
-	
+
+
 	$(".owl-filter-bar").on("click", ".owl-filter-bar-item", function () {
-		
+
 		var $item = $(this);
-		
+
 		var filter = $item.data("owl-filter");
-		
+
 		owl.owlcarousel2_filter(filter);
-		
+
 		$(".owl-filter-bar-item").removeClass("active");
-		
+
 		$item.addClass("active");
-		
-		
+
+
 	});
-	
-	
+
+
 	// Instagram Carousel
 	var owl2 = $(".owl-carousel.insta-slider").owlCarousel({
 		loop: true,
@@ -86,7 +86,7 @@ $(document).ready(function () {
 			}
 		}
 	});
-	
+
 	// Questions Carousel
 	var owl3 = $(".owl-carousel.question-examples-slider").owlCarousel({
 		loop: false,
@@ -105,54 +105,60 @@ $(document).ready(function () {
 			}
 		}
 	});
-	
+
 	//Faq Toogles
 	$(".faq-collapse .question").click(function () {
 		$(this).toggleClass("open");
 		$(this).next().slideToggle();
 	});
-	
+
+
+	$(".how_to-collapse .layout").click(function () {
+		$(this).find('.question').toggleClass("open");
+		$(this).find('.question').next().slideToggle();
+	});
+
 	// Modal Window
-	
+
 	$(".video-instruction-box-lg, .video-instruction-box ").on("click", function () {
 		console.log(".video-instruction-box-lg Click");
 		var video_wrapper = $(".youtube-video-place");
-		
+
 		if (video_wrapper.length) {
 			video_wrapper.html("<iframe width='100%' height='100%' allowfullscreen frameborder='0'" +
 				" class='embed-responsive-item'" +
 				" src='" + video_wrapper.data("yt-url") + "'></iframe>");
 		}
-		
-		
+
+
 		let modal_name = $(this).attr("data-modal");
-		
+
 		console.log(modal_name);
 		$("#" + modal_name).addClass("active");
 	});
-	
+
 	$(".close ").on("click", function () {
 		$("iframe").attr("src", $(this).find("iframe").attr("src"));
-		
+
 		$(".popup, .popup-content").removeClass("active");
 	});
-	
-	
-	
-	
+
+
+
+
 	$(".question-examples .get-answer").click(function () {
 		$(this).prev(".answer").toggle();
 	});
-	
-	
+
+
 	window.onscroll = function () {
 		myFunction();
 	};
-	
+
 	var header = document.getElementById("header");
-	
+
 	var sticky = header.offsetTop;
-	
+
 	function myFunction() {
 		if (window.pageYOffset > sticky) {
 			header.classList.add("sticky");
@@ -160,5 +166,29 @@ $(document).ready(function () {
 			header.classList.remove("sticky");
 		}
 	}
-	
+
+
+
+	if(window.innerWidth < 768 ) {
+		// Instagram Carousel
+		var owl_home_games = $(".section-info-grid .owl-carousel").owlCarousel({
+			loop: true,
+			// margin: 10,
+			center: false,
+			nav: true,
+			dots: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 2
+				}
+			}
+		});
+	}
+
 });
